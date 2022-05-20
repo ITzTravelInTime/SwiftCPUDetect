a5e5e91b692d61e1c8db0d256dcfccd4cbeb5435
ITzTravelInTime <pietrocaruso97@yahoo.it>
2022-05-20 02:00:45 +0200

    Removed the unused test target to allows the project to be compiled for watchOS using the Swift 5.3 toolchain and Xcode 12.

--- Package.swift
+++ Package.swift
@@ -27,9 +27,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftCPUDetect",
-            dependencies: []),
+            dependencies: [])
+        /*,
         .testTarget(
             name: "SwiftCPUDetectTests",
-            dependencies: ["SwiftCPUDetect"])
+            dependencies: ["SwiftCPUDetect"]) */
     ]
 )
