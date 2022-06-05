d1dab8fbc127e8192f2627902c34028efb0a49a1
ITzTravelIntime <23339020+ITzTravelInTime@users.noreply.github.com>
2022-06-05 06:25:17 +0200

    Updated the `SwiftPackagesBase` dependancy and simplified the implementation of the `Identifiable` protocol compliances.

--- Sources/SwiftCPUDetect/Extensions.swift
+++ Sources/SwiftCPUDetect/Extensions.swift
@@ -1,28 +1,13 @@
 
 import Foundation
+import SwiftPackagesBase
 
 #if !os(Linux)
 
-@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13, *) extension CpuArchitecture: Identifiable{
-    public var id: RawValue{
-        return rawValue
-    }
-}
-
-#endif
-
-#if !os(Linux)
-@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13, *) extension UnameReimplemented.UnameCommandLineArgs: Identifiable{
-    public var id: RawValue{
-        return rawValue
-    }
-}
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13, *) extension CpuArchitecture: Identifiable{}
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13, *) extension UnameReimplemented.UnameCommandLineArgs: Identifiable{}
 #endif
 
 #if os(macOS)
-@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13, *) extension AppExecutionMode: Identifiable{
-    public var id: RawValue{
-        return rawValue
-    }
-}
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13, *) extension AppExecutionMode: Identifiable{}
 #endif
