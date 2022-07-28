a2753e01e5c15292ba0881f828438df45acc7bdf
ITzTravelIntime <23339020+ITzTravelInTime@users.noreply.github.com>
2022-07-28 22:42:08 +0200

    The `CPUID.Registers` struct is now `Codable` and `Equatable` and allows for caching of the queries.

--- Sources/SwiftSystemValues/x86_CPUID.swift
+++ Sources/SwiftSystemValues/x86_CPUID.swift
@@ -13,45 +13,66 @@ import _Builtin_intrinsics.intel.cpuid
 import SwiftPackagesBase
 
 public final class CPUID{
-    public struct Registers: Copying{
+    public struct Registers: Copying, Codable, Equatable{
+        
+        public static var queryCache: [Self] = []
         
         public func copy() -> Self {
-            return .init(eax: eax, ebx: ebx, ecx: ecx, edx: edx)
+            return .init(eax: eax, ebx: ebx, ecx: ecx, edx: edx, queryLevel1: queryLevel1, queryLevel2: queryLevel2)
         }
         
+        ///The contents of the EAX register after the CPUID instruction
         public let eax: UInt32
+        ///The contents of the EBX register after the CPUID instruction
         public let ebx: UInt32
+        ///The contents of the ECX register after the CPUID instruction
         public let ecx: UInt32
+        ///The contents of the EDX register after the CPUID instruction
         public let edx: UInt32
         
-        ///Returns the current registers state after performing the cpuid function for the specified level
-        public static func from(level: UInt32) -> Self?{
-            
-            var eax: UInt32 = 0
-            var ebx: UInt32 = 0
-            var ecx: UInt32 = 0
-            var edx: UInt32 = 0
-            
-            if __get_cpuid(level, &eax, &ebx, &ecx, &edx) == 0{
-                return nil
-            }
-            
-            return .init(eax: eax, ebx: ebx, ecx: ecx, edx: edx)
-        }
+        ///The contents of the EAX register before the CPUID instruction
+        public let queryLevel1: UInt32
         
-        ///Returns the current registers state after performing the cpuid function for the specified level
-        public static func from(level: UInt32, extendedLevel: UInt32) -> Self?{
+        ///The contents of the ECX register before the CPUID instruction
+        public let queryLevel2: UInt32?
+        
+        ///Returns the current registers state after performing the cpuid function for the specified levels
+        public static func from(level: UInt32, extendedLevel: UInt32? = nil, storeQueryInCache: Bool = true, recoverFromCache: Bool = false) -> Self?{
+            
+            if recoverFromCache{
+                var ret: Self? = nil
+                
+                for data in queryCache where data.queryLevel1 == level && data.queryLevel2 == extendedLevel {
+                    ret = data
+                }
+                
+                if let data = ret{
+                    return data
+                }
+            }
             
             var eax: UInt32 = 0
             var ebx: UInt32 = 0
             var ecx: UInt32 = 0
             var edx: UInt32 = 0
             
-            if __get_cpuid_count(level, extendedLevel, &eax, &ebx, &ecx, &edx) == 0{
+            if let extended = extendedLevel{
+                
+                if __get_cpuid_count(level, extended, &eax, &ebx, &ecx, &edx) == 0{
+                    return nil
+                }
+                
+            } else if __get_cpuid(level, &eax, &ebx, &ecx, &edx) == 0{
                 return nil
             }
             
-            return .init(eax: eax, ebx: ebx, ecx: ecx, edx: edx)
+            let ret = Self.init(eax: eax, ebx: ebx, ecx: ecx, edx: edx, queryLevel1: level, queryLevel2: extendedLevel)
+            
+            if storeQueryInCache{
+                queryCache.append(ret)
+            }
+            
+            return ret
         }
     }
     
