513f8f6ff0615c0643887b288cc2cd72f5b19b68
ITzTravelIntime <23339020+ITzTravelInTime@users.noreply.github.com>
2023-05-25 11:04:59 +0200

    Add/update copyright+license comments

--- Sources/SwiftCPUDetect/Extensions.swift
+++ Sources/SwiftCPUDetect/Extensions.swift
@@ -1,7 +1,7 @@
 
 /*
  SwiftCPUDetect a Swift library to collect system and current process info.
- Copyright (C) 2022 Pietro Caruso
+ Copyright (C) 2022-2023 Pietro Caruso
 
  This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
 
